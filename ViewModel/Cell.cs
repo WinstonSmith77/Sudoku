cbf4ee136a6f77c75dd038a3865da8d424ab63cc
WinstonSmith77 <matze_henning@gmx.de>
2014-02-13 23:40:23 +0100

    UI gets better

--- ViewModel/Cell.cs
+++ ViewModel/Cell.cs
@@ -9,35 +9,34 @@ using ViewModel.Annotations;
 
 namespace ViewModel
 {
-    public class Field : INotifyPropertyChanged
+    public class Cell : INotifyPropertyChanged
     {
-       
+        public const int Start = 1;
+        public const int Width = 9;
+
+        public Cell()
+        {
+            Values = Enumerable.Range(Start, Width).ToArray();
+            Result = "?";
+        }
+
+        public int[] Values
+        {
+            get;
+            set;
+        }
+
+        public string Result { get; set; }
+
+
         public event PropertyChangedEventHandler PropertyChanged;
-
-        public Field()
-        {
-            Cells = new Cell[Cell.Width * Cell.Width];
-
-            for (int x = 0; x < Cell.Width; x++)
-            {
-                for (int y = 0; y < Cell.Width; y++)
-                {
-                    Cells[x + y *Cell.Width] = new Cell();
-                }
-            }
-        }
-
-        public Cell[] Cells
-        {
-            get; 
-            private set;
-        }
 
         [NotifyPropertyChangedInvocator]
         protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
             PropertyChangedEventHandler handler = PropertyChanged;
             if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+
         }
     }
 }
